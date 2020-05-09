f547dab2e75f8550416769a28678e569051efa82
Simon Ledoux <simon@simon511000.fr>
2020-05-09 19:08:49 +0200

    Changement temporaire du serveur en raison d'une maintenance

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://citycraft.simon511000.fr/launcher/distribution.json'
+        const distroURL = 'http://82.64.35.245:25567/launcher/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
